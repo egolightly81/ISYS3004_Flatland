ed0ac1e0ca1be72579cfcda1d9c04bd0e40beb63
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-08 09:04:15 +0800

    removed let replaced with var

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ Buzzword Phrase Generator (JavaScript)
 
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
-    let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
-    let action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
-    let outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
+    var buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
+    var action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
+    var outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
 
-    let idx_buz = Math.floor(Math.random() * buzz.length);
-    let idx_act = Math.floor(Math.random() * action.length);
-    let idx_out = Math.floor(Math.random() * outcome.length);
+    var idx_buz = Math.floor(Math.random() * buzz.length);
+    var idx_act = Math.floor(Math.random() * action.length);
+    var idx_out = Math.floor(Math.random() * outcome.length);
 
     return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
 }
