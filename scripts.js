60adbbd73f8e97df645d2cdbf6b00e3ea36ecad5
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-08 09:49:33 +0800

    added fix

--- scripts.js
+++ scripts.js
@@ -1,21 +1,14 @@
-// 1. Locate the Element(s)
-var square = document.getElementById('square')
+var square = document.getElementById('square');
 
-// 2. Write the functionality needed when event occurs 
-function changeColour(Colour){
-        square.style.background = 'Colour';
-    }
-//3. Link element, event to functionality 
-// i.e add an event listener
-square.addEventListener('click', (event)=> changeColour('green'))
-square.addEventListener('mouseover', (event)=> changeColour('red'))
-square.addEventListener('mouseout', (event)=> changeColour('grey'))
+function changeColour(Colour) {
+    square.style.background = Colour;
+}
 
-
-// this needs work - re watch the lecture 
+square.addEventListener('click', (event) => changeColour('green'));
+square.addEventListener('mouseover', (event) => changeColour('red'));
+square.addEventListener('mouseout', (event) => changeColour('grey'));
 
 function createBuzzwordPhrase() {
-    /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     var buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
     var action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
     var outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
@@ -24,5 +17,5 @@ function createBuzzwordPhrase() {
     var idx_act = Math.floor(Math.random() * action.length);
     var idx_out = Math.floor(Math.random() * outcome.length);
 
-    return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
+    return buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
 }
