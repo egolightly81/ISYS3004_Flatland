24b58c7687230d76eae24ba987d8596636265e69
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-08 14:09:10 +0800

    trouble shooting errors

--- scripts.js
+++ scripts.js
@@ -1,26 +1,25 @@
-// 1. Locate the Element(s)
-var square = document.getElementById('square')
-
-// 2. Write the functionality needed when event occurs 
-function changeColour(Colour){
-        square.style.background = 'Colour';
-    }
-//3. Link element, event to functionality 
-// i.e add an event listener
-square.addEventListener('click', (event)=> changeColour('green'))
-square.addEventListener('mouseover', (event)=> changeColour('red'))
-square.addEventListener('mouseout', (event)=> changeColour('grey'))
-
 var square = document.getElementById('square');
 var words = document.getElementById('words');
 
 function changeColour(Colour) {
+    console.log('Changing colour to:', Colour);
     square.style.background = Colour;
 }
 
-square.addEventListener('click', (event) => changeColour('green'));
-square.addEventListener('mouseover', (event) => changeColour('red'));
-square.addEventListener('mouseout', (event) => changeColour('grey'));
+square.addEventListener('click', (event) => {
+    console.log('Clicked on square');
+    changeColour('green');
+});
+
+square.addEventListener('mouseover', (event) => {
+    console.log('Mouse over square');
+    changeColour('red');
+});
+
+square.addEventListener('mouseout', (event) => {
+    console.log('Mouse out of square');
+    changeColour('grey');
+});
 
 function createBuzzwordPhrase() {
     var buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
@@ -31,10 +30,13 @@ function createBuzzwordPhrase() {
     var idx_act = Math.floor(Math.random() * action.length);
     var idx_out = Math.floor(Math.random() * outcome.length);
 
-    return buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
+    var phrase = buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
+    console.log('Generated phrase:', phrase);
+    return phrase;
 }
 
 // Example usage of createBuzzwordPhrase function
 words.textContent = createBuzzwordPhrase();
 
-// trouble shoot https://chat.openai.com/share/8017b117-d659-4873-b3a3-ed07550957e8 
+
+// trouble shoot https://chat.openai.com/share/8017b117-d659-4873-b3a3-ed07550957e8
